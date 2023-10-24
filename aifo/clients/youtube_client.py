e72f3c8ff33223a4679e1ab58b43ea1713048b34
m@shubi.ch <m@shubi.ch>
2023-10-24 19:03:06 +0200

    added first youtube client func

---
+++ aifo/clients/youtube_client.py
@@ -0,0 +1,35 @@
+from pytube import YouTube
+from pytube.exceptions import VideoUnavailable
+import logging
+# https://pytube.io/en/latest/
+
+
+def on_progress(stream, chunks, bytes_remaining):
+    return
+
+def on_completion():
+    return
+
+def Download(link, path):
+    log = logging.getLogger("rich")
+    log.info(f"Attemting to download {link}")
+    try:
+        yt = YouTube(
+            link,
+            on_progress_callback=on_progress,
+            use_oauth=False,
+            allow_oauth_cache=False
+        )
+        log.debug("video information")
+        log.debug(f"title={yt.title}")
+        log.debug(f"description={yt.description}")
+        log.debug(f"views={yt.views}")
+        log.debug(f"publish_date={yt.publish_date}")
+        log.debug(f"keywords={yt.keywords}")
+        log.debug(f"age_restricted={yt.age_restricted}")
+    except VideoUnavailable as e:
+        log.error("the video is unavailable")
+        log.exception(e)
+    else:
+        log.info("downloading video")
+        yt.streams.get_highest_resolution().download(path)
--- aifo/main.py
+++ aifo/main.py
@@ -2,8 +2,10 @@ import os
 import tkinter
 import argparse
 import logging
+
 from rich.logging import RichHandler
 from aifo.gui.ChatWindow import ChatWindow
+from aifo.clients.youtube_client import Download
 
 
 API_KEY = "/home/musa/gclk/aifo-project1-dac1e93933c0.json"
@@ -19,26 +21,43 @@ def main():
     log = logging.getLogger("rich")
     
     parser = argparse.ArgumentParser(
-                    prog='AIFO - AI Youtube Media Downloader',
-                    description='Download Youtube Media with AI',
-                    epilog='Enjoy the program! :)')
-    parser.add_argument("--api_key",
-                        type=str,
-                        help="Path to API key json file",
-                        default=API_KEY)
-    parser.add_argument("--cli_mode",
-                        type=bool,
-                        help="Start program in CLI mode",
-                        default=False)
+        prog='AIFO - AI Youtube Media Downloader',
+        description='Download Youtube Media with AI',
+        epilog='Enjoy the program! :)'
+    )
+    parser.add_argument(
+        "--api_key",
+        type=str,
+        default=API_KEY,
+        help="Path to API key json file"
+    )
+    parser.add_argument(
+        "--cli_mode",
+        "-c",
+        action="store_true",
+        help="Start program in CLI mode"
+    )
+    parser.add_argument(
+        "--verbose", 
+        "-v", 
+        action="store_true", 
+        help="Enable verbose output"
+    )
     args = parser.parse_args()
-    log.debug(args)
+    
+    if args.verbose:
+        log.level = 10
+        log.debug("enabled verbose logging")
+
     log.info(f"setting API key env: GOOGLE_APPLICATION_CREDENTIALS={args.api_key}")
-    os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = API_KEY
+    os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = args.api_key
     log.info(f"starting mode: cli_mode={args.cli_mode}")
+
     if args.cli_mode:
         start_cli_mode
     else:
         start_gui_mode()
+        return
 
 # start program in gui mode
 def start_gui_mode():
